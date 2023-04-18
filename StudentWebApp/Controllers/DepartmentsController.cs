0d249773cc6ec62e0a92cc7e9a768e358cbd41d6
Mehedi Hasan Tushar <tushar.it@labaidpharma.com>
2023-04-18 12:42:56 +0600

    New controller added

---
+++ StudentWebApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        string d = "Hello";
+        
+
+    }
+}
